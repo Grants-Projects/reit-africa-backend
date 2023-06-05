7a4cb761985ab2fafe646f83f9adf13cb46e3bb5
marvikomo <marvikomo@gmail.com>
2023-06-05 19:50:25 +0100

    added workflow for generating client

---
+++ generate.sh
@@ -0,0 +1,5 @@
+echo "Generating clients`'
+java -jar openapi-generator-cli.jar generate \
+            -i ./spec/api-spec.yaml \
+            -g typescript-fetch \
+            -o ./generated-clients
