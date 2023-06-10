78d0fca305e3be8c5ac3dc59a5e545ba2cbec405
erin deji <erin.deji@gmail.com>
2023-06-10 09:41:12 +0100

    updates

--- src/routes/v1/auth.route.ts
+++ src/routes/v1/auth.route.ts
@@ -6,6 +6,6 @@ import { AuthController } from '../../controller/auth.controller';
 
 const authController: AuthController = container.resolve(AuthController);
 router.post('/login', authController.loginUser)
-.get('/verify-user', authController.verifyUser);
+.get('/verify-user-exists', authController.verifyUser);
 
 export default router;
